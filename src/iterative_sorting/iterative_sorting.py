ad89d20523607fe2bdfee9d4dfeb8a225fb14184
Lambda_School_Loaner_98 <henry.neal0@gmail.com>
2019-04-29 16:32:56 -0400

    solution started

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,12 +4,35 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        smallest = min(arr[i:])
+        smallest_index = arr.index(smallest)
+        if arr[smallest_index] < arr[cur_index]:
+            arr[cur_index] = arr[smallest_index]
+
+
+        
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        # loop through array, find smallest value
+        # for j in range(i + 1, len(arr) - 1):
+        #     if arr[j] < arr[cur_index]:
+        #         j = cur_index
 
+                
 
-
+            # smallest = min(arr[j:])
+            # index = arr.index(smallest)
+            # if smallest < arr[i]:
+            #     arr[i] = smallest
+            #     arr[index] = arr[i]
+                
+        # move smallest value to beginning of array
+        # swap OG first element to smallest value's OG place
+        # loop through array, starting 1 index in
+        
+        # min_array = min(arr[i:len(arr) - 1])
+        # min_index = arr.index(min_array)
+        
         # TO-DO: swap
 
 
